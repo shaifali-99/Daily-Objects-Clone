8d2fdf47accf305aef6d3c2ae84931c27607d4fd
shaifali <shaifali02000@gmail.com>
2023-02-21 20:54:04 +0530

    deletedLogin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Login } from './Pages/LogIn/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Login/>
     </div>
   );
 }

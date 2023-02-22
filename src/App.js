68f0dec7dd87ef16f5bb7c834cbbc202031010c9
shaifali <shaifali02000@gmail.com>
2023-02-22 11:50:56 +0530

    checkOnce2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import { Login } from './Pages/UserAccount/Login';
-import { Wishlist } from './Pages/UserAccount/WishList';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Login/>
-      <Wishlist/>
     </div>
   );
 }

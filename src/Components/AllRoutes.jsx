15aa749edff69ef0c8e91e50dff5a30c775388a0
GauravSingh72 <1gaurav2singh@gmail.com>
2023-02-22 21:57:58 +0530

    Navbar Added

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Cases from '../Pages/Cases and sleeves/Cases'
+// import Cases from '../Pages/Cases and sleeves/Cases'
 import Home from '../Pages/Home/Home'
 import FilterMobiles from './Cases and covers/FilterMobiles'
 import PhoneCovers from './Cases and covers/GroupSingle/Iphone.jsx/PhoneCovers'

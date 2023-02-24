f35df771e39aa76ad9dbe17f5faa9435ac3e7f46
Amit bhandari <113005542+amit9675@users.noreply.github.com>
2023-02-24 20:32:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 
-import axios from 'axios';
-import { useEffect, useState } from "react";
 import "./App.css";
 import AllRoutes from "./Components/AllRoutes";
 import Footer from "./Components/Footer";

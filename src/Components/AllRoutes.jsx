5a012decc5d0382522a9338f0b8e22e1c71b72c5
Amit bhandari <113005542+amit9675@users.noreply.github.com>
2023-02-24 20:27:44 +0530

    Update AllRoutes.jsx

--- src/Components/AllRoutes.jsx
+++ src/Components/AllRoutes.jsx
@@ -15,35 +15,9 @@ import LaptopCoverNavbar from "./Laptop Macbook Sleeves/GroupSingle/LaptopCoverN
 import MacbookSleeves from "./Laptop Macbook Sleeves/GroupSingle/MacbookSleeves";
 import Messenger from "./Laptop Macbook Sleeves/GroupSingle/Messenger";
 import ZippedSleeves from "./Laptop Macbook Sleeves/GroupSingle/ZippedSleeves";
-
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Cases from "../Pages/Cases and sleeves/Cases";
-
-import { Route, Routes } from 'react-router-dom'
-import Cases from '../Pages/Cases and sleeves/cases'
-// import Home from '../Pages/Home/Home'
-// import PhoneCovers from './Cases and covers/GroupSingle/PhoneCovers'
-// import Cases from '../Pages/Cases and sleeves/cases'
-// import Cases from '../Pages/Cases and sleeves/Cases'
-import Home from '../Pages/Home/Home'
-import { AccountPage } from '../Pages/UserAccount/AccountPage'
-import { Rewards } from '../Pages/UserAccount/Reward'
-import FilterMobiles from './Cases and covers/FilterMobiles'
-import PhoneCovers from './Cases and covers/GroupSingle/Iphone.jsx/PhoneCovers'
-import OppoCover from './Cases and covers/GroupSingle/Oppo/OppoCover'
-import NothingCover from './Cases and covers/Nothing/NothingCover'
-import OneplusCover from './Cases and covers/Onepus/OneplusCover'
-import SamsumgCover from './Cases and covers/Samsung/SamsumgCover'
-import VivoCover from './Cases and covers/Vivo/VivoCover'
-import All from './Laptop Macbook Sleeves/GroupSingle/All'
-import Envelop from './Laptop Macbook Sleeves/GroupSingle/Envelop'
-import LaptopCoverNavbar from './Laptop Macbook Sleeves/GroupSingle/LaptopCoverNavbar'
-import MacbookSleeves from './Laptop Macbook Sleeves/GroupSingle/MacbookSleeves'
-import Messenger from './Laptop Macbook Sleeves/GroupSingle/Messenger'
-import ZippedSleeves from './Laptop Macbook Sleeves/GroupSingle/ZippedSleeves'
-
-
 import NewArrivalComp from "./NewArrivalComp/NewArrivalComp";
 import NewArrivalsPage from "../Pages/NewArrivalsPage.jsx/NewArrivalsPage";
 import AllAccessories from "./Accessories/AllAccessories";
@@ -69,8 +43,7 @@ export default function AllRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/cases" element={<Cases />} />
         <Route path="/cases/phonecovers" element={<FilterMobiles />} />
-        <Route path='/rewards' element={<Rewards/>}/>
-        <Route path='/account/*' element={<AccountPage/>}/>
+      
         <Route path='/cases/phonecovers/mobiles' element={<FilterMobiles/>}/>
 
         <Route path="/cases/phonecovers/apple" element={<PhoneCovers />} />
@@ -90,23 +63,13 @@ export default function AllRoutes() {
         <Route path="/cases/laptopcovers/messenger" element={<Messenger />} />
         <Route path="/account/*" element={<AccountPage />} />
         <Route path="/rewards" element={<Rewards />} />
-
-        {/* <Route path='/cases/phonecovers' element={<PhoneCovers/>}/> */}
-      {/* </Routes>
-
-      <Routes>
-        <Route path="/cases/phonecovers" element={<FilterMobiles />} /> */}
-        {/* <Route path='/rewards' element={<Rewards/>}/> */}
         <Route path='/cases/phonecovers' element={<FilterMobiles/>}/>
-        {/* <Route path="/cases/phonecovers/apple" element={<PhoneCovers />} /> */}
-
         <Route
           path="/iphoneCovers/:id"
           element={<IndividualPhoneCoverData />}
         />
         <Route path="/newarrivalspage" element={<NewArrivalsPage />}></Route>
         <Route path="/newarrivalcomp" element={<NewArrivalComp />}></Route>
-        {/* <Route path='/cases/phonecovers' element={<PhoneCovers/>}/> */}
         <Route path="/accesories" element={<AllAccessories />} />
         <Route path="/sale" element={<Sale />} />
         <Route path="/wallets" element={<AllAccessories />} />

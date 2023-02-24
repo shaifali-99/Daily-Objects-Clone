d2aeed0736651ca901125c1ede388c9444018195
Amit bhandari <113005542+amit9675@users.noreply.github.com>
2023-02-24 20:31:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 
 import axios from 'axios';
-import { useEffect, useState } from 'react';
-import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
 import AllRoutes from "./Components/AllRoutes";
